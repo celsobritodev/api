e4f4d7dec55d1e07ea458662a2ba27d5b785e1bb
celsobritodev <celsinhobrito@gmail.com>
2025-10-15 20:21:09 -0300

    teste da classe UserService / metodo create com excessao

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.services.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,11 +16,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 public class UserServiceImplTest {
 	
+	private static final int INDEX = 0;
+	private static final String OBJETO_NÃO_ENCONTRADO = "Objeto não encontrado!";
 	private static final Integer ID = 1;
 	private static final String PASSWORD = "123";
 	private static final String EMAIL = "helder@gmail.com";
@@ -68,16 +72,72 @@ public class UserServiceImplTest {
 	@Test
 	void whenFindByIdThenReturnAnObjectNotFoundException() {
 		Mockito.when(repository.findById(Mockito.anyInt()))
-			.thenThrow(new ObjectNotFoundException("Objeto não encontrado!"));
+			.thenThrow(new ObjectNotFoundException(OBJETO_NÃO_ENCONTRADO));
 		try {
 			service.findById(ID);
 		} catch (Exception ex) {
 			Assertions.assertEquals(ObjectNotFoundException.class, ex.getClass());
-			Assertions.assertEquals("Objeto não encontrado!", ex.getMessage());
+			Assertions.assertEquals(OBJETO_NÃO_ENCONTRADO, ex.getMessage());
 		
 		}
 		
 	}
+	
+	
+	@Test
+	void whenFindAllThenReturnAnListOfUsers() {
+		Mockito.when(repository.findAll()).thenReturn(List.of(user));
+		
+		List<User> users = service.findAll();
+		
+		Assertions.assertNotNull(users); // verifica se o objeto não é nulo
+		Assertions.assertEquals(1, users.size()); // verifica se o tamanho da lista é igual a 1
+		Assertions.assertEquals(User.class, users.get(INDEX).getClass());
+		
+		Assertions.assertEquals(ID, users.get(INDEX).getId());
+		Assertions.assertEquals(NAME, users.get(INDEX).getName());
+		Assertions.assertEquals(EMAIL, users.get(INDEX).getEmail());
+		Assertions.assertEquals(PASSWORD, users.get(INDEX).getPassword());
+		
+	}
+	
+	
+	@Test
+	void whenCreateThenReturnSuccess() {
+		Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+		
+		User userResponse = service.create(userDTO);
+		
+		Assertions.assertNotNull(userResponse); // verifica se o objeto não é nulo
+		Assertions.assertEquals(User.class, userResponse.getClass()); // verifica se o objeto retornado é da classe User
+		Assertions.assertEquals(ID, userResponse.getId());
+		Assertions.assertEquals(NAME, userResponse.getName());
+		Assertions.assertEquals(EMAIL, userResponse.getEmail());
+		Assertions.assertEquals(PASSWORD, userResponse.getPassword());
+		
+	}
+	
+	
+	@Test
+	void whenCreateThenReturnAnDataIntegrityViolationException() {
+		// Quando o método findByEmail do repositório for chamado com qualquer string, retorne o optionalUser
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(optionalUser);
+		
+		try {
+			optionalUser.get().setId(2); // altera o id do usuário para simular que já existe outro usuário com o mesmo email
+			service.create(userDTO);
+		} catch (Exception ex) {
+			Assertions.assertEquals(DataIntegratyViolationException.class, ex.getClass());
+			Assertions.assertEquals("Email já cadastrado no sistema!", ex.getMessage());
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 	private void startUser() {
 		user = new User(ID, NAME, EMAIL, PASSWORD);
