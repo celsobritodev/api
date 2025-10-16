e39493b6b45c96ba7902927ef8f9367bb1d8632c
celsobritodev <celsinhobrito@gmail.com>
2025-10-15 22:00:38 -0300

    teste da classe UserService / metodo update com excessao

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -22,6 +22,7 @@ import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 @SpringBootTest
 public class UserServiceImplTest {
 	
+	private static final String EMAIL_JÁ_CADASTRADO_NO_SISTEMA = "Email já cadastrado no sistema!";
 	private static final int INDEX = 0;
 	private static final String OBJETO_NÃO_ENCONTRADO = "Objeto não encontrado!";
 	private static final Integer ID = 1;
@@ -83,7 +84,7 @@ public class UserServiceImplTest {
 		
 	}
 	
-	
+	// Teste para o método findAll
 	@Test
 	void whenFindAllThenReturnAnListOfUsers() {
 		Mockito.when(repository.findAll()).thenReturn(List.of(user));
@@ -128,7 +129,42 @@ public class UserServiceImplTest {
 			service.create(userDTO);
 		} catch (Exception ex) {
 			Assertions.assertEquals(DataIntegratyViolationException.class, ex.getClass());
-			Assertions.assertEquals("Email já cadastrado no sistema!", ex.getMessage());
+			Assertions.assertEquals(EMAIL_JÁ_CADASTRADO_NO_SISTEMA, ex.getMessage());
+		}
+		
+		
+		
+	}
+	
+	
+	
+	@Test
+	void whenUpdateThenReturnSuccess() {
+		Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+		
+		User userResponse = service.update(userDTO);
+		
+		Assertions.assertNotNull(userResponse); // verifica se o objeto não é nulo
+		Assertions.assertEquals(User.class, userResponse.getClass()); // verifica se o objeto retornado é da classe User
+		Assertions.assertEquals(ID, userResponse.getId());
+		Assertions.assertEquals(NAME, userResponse.getName());
+		Assertions.assertEquals(EMAIL, userResponse.getEmail());
+		Assertions.assertEquals(PASSWORD, userResponse.getPassword());
+		
+	}
+	
+	
+	@Test
+	void whenUpdateThenReturnAnDataIntegrityViolationException() {
+		// Quando o método findByEmail do repositório for chamado com qualquer string, retorne o optionalUser
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(optionalUser);
+		
+		try {
+			optionalUser.get().setId(2); // altera o id do usuário para simular que já existe outro usuário com o mesmo email
+			service.create(userDTO);
+		} catch (Exception ex) {
+			Assertions.assertEquals(DataIntegratyViolationException.class, ex.getClass());
+			Assertions.assertEquals(EMAIL_JÁ_CADASTRADO_NO_SISTEMA, ex.getMessage());
 		}
 		
 		
